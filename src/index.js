eee9e13c0ba5feba7283f20af4d60afc2e9e3582
Nicolas Mayorga <namayorgav@gmail.com>
2022-11-18 23:54:48 -0500

    Commit until JSX JavaScript topic

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDom from "react-dom"
+//CSS
+import "./index.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+/* Stateless functional component
+   Always return JSX */
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/* JSX Rules
+Return Single element
+div / section / article or React.Fragment => <> </>
+use camelCase for html attribute
+className instead of class
+close every element
+formatting */
+
+// Nested Components, React Tools
+
+const App = () => {
+  return (
+    <>
+      <h1 className="title">iBooklist</h1>
+      <section className="container">
+        <Book />
+        <Book />
+        <Book />
+        <Book />
+        <Book />
+        <Book />
+        <Book />
+      </section>
+    </>
+  )
+}
+
+const Book = () => {
+  const title = "Fairy Tale"
+  const author = "Stephen King"
+
+  return (
+    <div className="card">
+      <img
+        src="https://i0.wp.com/ellaberintodelminotauro.com.co/wp-content/uploads/2022/02/fairy-tale-9781668002179_xlg.jpg?ssl=1"
+        alt="Fairy Tale book - Stephen King"
+      />
+      <h2 className="title">{title}</h2>
+      <h4 className="author">{author}</h4>
+    </div>
+  )
+}
+
+ReactDom.render(<App />, document.getElementById("root"))
