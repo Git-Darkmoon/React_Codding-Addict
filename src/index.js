5e02dcdd72ccb6c4735db709b2bb1771dee24f34
Nicolas Mayorga <namayorgav@gmail.com>
2022-11-21 21:26:44 -0500

    Progress until Props Destructuring

--- src/index.js
+++ src/index.js
@@ -16,33 +16,70 @@ formatting */
 
 // Nested Components, React Tools
 
+const bookProps = [
+  {
+    image:
+      "https://i0.wp.com/ellaberintodelminotauro.com.co/wp-content/uploads/2022/02/fairy-tale-9781668002179_xlg.jpg?ssl=1",
+    title: "Fairy Tale",
+    author: "Stephen King",
+  },
+  {
+    image:
+      "https://storage.googleapis.com/du-prd/books/images/9781668001226.jpg",
+    title: "IT STARTS WITH US",
+    author: "Colleen Hoover",
+  },
+  {
+    image:
+      "https://storage.googleapis.com/du-prd/books/images/9780316485654.jpg",
+    title: "DESERT STAR",
+    author: "Michael Connelly",
+  },
+  {
+    image:
+      "https://storage.googleapis.com/du-prd/books/images/9781501110375.jpg",
+    title: "IT ENDS WITH US",
+    author: "Colleen Hoover",
+  },
+]
+
 const App = () => {
   return (
     <>
       <h1 className="title">iBooklist</h1>
       <section className="container">
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
+        <Book
+          image={bookProps[0].image}
+          title={bookProps[0].title}
+          author={bookProps[0].author}
+        />
+        <Book
+          image={bookProps[1].image}
+          title={bookProps[1].title}
+          author={bookProps[1].author}
+        />
+        <Book
+          image={bookProps[2].image}
+          title={bookProps[2].title}
+          author={bookProps[2].author}
+        />
+        <Book
+          image={bookProps[3].image}
+          title={bookProps[3].title}
+          author={bookProps[3].author}
+        />
       </section>
     </>
   )
 }
 
-const Book = () => {
-  const title = "Fairy Tale"
-  const author = "Stephen King"
+const Book = ({ image, title, author }) => {
+  /* ES6 Object destructuring
+  const { image, title, author } = props */
 
   return (
     <div className="card">
-      <img
-        src="https://i0.wp.com/ellaberintodelminotauro.com.co/wp-content/uploads/2022/02/fairy-tale-9781668002179_xlg.jpg?ssl=1"
-        alt="Fairy Tale book - Stephen King"
-      />
+      <img src={image} alt={title + "-" + author} />
       <h2 className="title">{title}</h2>
       <h4 className="author">{author}</h4>
     </div>
