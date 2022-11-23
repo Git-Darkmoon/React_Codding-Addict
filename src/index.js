72f63bb8a2666339edc18d35189e932dc3dc4fc5
Nicolas Mayorga <namayorgav@gmail.com>
2022-11-22 21:58:16 -0500

    Progress until Event Basics

--- src/index.js
+++ src/index.js
@@ -57,17 +57,32 @@ const App = () => {
   )
 }
 
-const Book = (props) => {
-  /* ES6 Object destructuring, also can be done in the parameter */
-  const { image, title, author } = props
+const Book = ({ image, title, author }) => {
+  /* ES6 Object destructuring, also can be done in the parameter
+  const { image, title, author } = props */
 
   return (
     <div className="card">
       <img src={image} alt={title + "-" + author} />
       <h2 className="title">{title}</h2>
       <h4 className="author">{author}</h4>
+      <div className="btns">
+        <button className="btn" onClick={showMessage}>
+          Click me !
+        </button>
+        <button
+          className="btn"
+          onClick={() => alert(`This book is called: ${title}`)}
+        >
+          Show title
+        </button>
+      </div>
     </div>
   )
+}
+
+const showMessage = () => {
+  alert("Welcome to React !")
 }
 
 ReactDom.render(<App />, document.getElementById("root"))
